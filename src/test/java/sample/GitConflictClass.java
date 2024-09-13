cddc16e2adb417412ad99b3d3f1511a8589fda7a
Nikhil A <Nikhil A@Nikhil>
2024-09-13 10:25:06 +0530

    This is gitconflict

---
+++ src/test/java/sample/GitConflictClass.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class GitConflictClass {
+@Test
+public void gitconflictMethod()
+{
+	System.out.println("Learning Git conflict");
+}
+
+}
